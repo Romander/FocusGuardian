00eef4c98dfabe4d4c60f5a4cd7c8de7cc8e96a7
Roman Ogoreltsev <romanderyo@gmail.com>
2024-12-26 15:52:09 +0100

    add fr, de and ch localization + fix hindi (#3)
    Co-authored-by: Roman Ogoreltcev <Roman_Ogoreltcev@epam.com>

--- src/types/lang.ts
+++ src/types/lang.ts
@@ -1,2 +1,2 @@
-export const LANGUAGES = ["en", "ru", "tr", "hi-IN"] as const;
+export const LANGUAGES = ["en", "ru", "tr", "hi", "de", "fr", "zh_CN"] as const;
 export type LangType = (typeof LANGUAGES)[number];
